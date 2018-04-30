53ff87c78d2a8c88dc10726fadf863e5f0e2efa3
Philipp Martin <philipp.martin1@rwth-aachen.de>
2018-04-30 14:19:06 +0200

    non-database prototype done?

--- websource/scripts.js
+++ websource/scripts.js
@@ -1,6 +1,7 @@
 var stage=0; 
 var players={"East":"None","South":"None","West":"None","North":"None"};
 var points={"East":25000,"South":25000,"West":25000,"North":25000};
+var chombos={"East":0,"South":0,"West":0,"North":0};
 
 var sanma=false;
 
@@ -14,10 +15,33 @@ var honba=0;
 var round=0; //0=east,1=south,2=west,3=north etc.
 var kyoku=1;
 
-var yakuvals={ "tanyao":1, "chanta":2, "honrou":2, "junchan":3, "haku":1, "hatsu":1, "chun":1, "jikaze":1, "bakaze":1, "shousangen":2, "pinfu":1 "iipeikou":1, "sanshokudoujun":2, "ittsu":1, "ryanpeikou":2 "toitoi":2, "sanankou":2, "sanshokudoukou":2, "chankan":1, "rinshan":1, "sankantsu":2, "honitsu":3, "chinitsu":6, "doubleriichi":1, "renhou":5, "haiteihoutei":1, "chiitoi":2, "riichi":1, "openriichi":1, "nagashi":5, "kokushi":13, "tsuuiisou":13, "chinroutou":13, "daisangen":13, "shousuushi":13, "daisuushi":13, "chuuren":13, "suuankou":13, "suukantsu":13, "ryuuiisou":13, "chiihou":13, "tenhou":13};
+var yakuvals={ "tanyao":1, "chanta":2, "honrou":2, "junchan":3, "haku":1, "hatsu":1, "chun":1, "jikaze":1, "bakaze":1, "shousangen":2, "pinfu":1, "iipeikou":1, "sanshokudoujun":2, "ittsu":1, "ryanpeikou":2,"toitoi":2, "sanankou":2, "sanshokudoukou":2, "chankan":1, "rinshan":1, "sankantsu":2, "honitsu":3, "chinitsu":6, "doubleriichi":1, "renhou":5, "haiteihoutei":1, "chiitoi":2, "riichi":1, "openriichi":1, "nagashi":5, "kokushi":13, "tsuuiisou":13, "chinroutou":13, "daisangen":13, "shousuushi":13, "daisuushi":13, "chuuren":13, "suuankou":13, "suukantsu":13, "ryuuiisou":13, "chiihou":13, "tenhou":13};
 var kuisagari=["chanta","junchan","sanshokudoujun","honitsu","chintisu"];
 var closedonly=["pinfu","ittsu","iipeikou","ryanpeikou","renhou","riichi","openriichi","nagashi","kokushi","chuuren","suuankou","chiihou","tenhou"];
 
+var time=0;
+var interval;
+
+function update_time()
+{
+	time+=1;
+
+	var seconds = time;
+	var hours = Math.floor(seconds/3600);
+	seconds = seconds - hours*3600;
+	var minutes = Math.floor(seconds/60);
+	seconds = seconds - minutes*60;
+
+	document.getElementById("timer").innerHTML = hours + ":" + zeropad(minutes) + ":" + zeropad(seconds);
+}
+
+function zeropad(str)
+{
+	if(Number(str) < 10)
+		return "0"+str;
+	else
+		return str;
+}
 
 function update_display()
 {
@@ -34,37 +58,63 @@ function update_display()
 	document.getElementById("kyokucount").innerHTML = kyoku;
 
 	//honba
-	document.getElemnetById("honbacount").innerHTML = honba;
+	document.getElementById("honbacount").innerHTML = honba;
 	
 	//player winds
-	switch(kyoku)
+	if(sanma)
 	{
-		case 1:
-			document.getElementById("p1wind").innerHTML = "東";
-			document.getElementById("p2wind").innerHTML = "南";
-			document.getElementById("p3wind").innerHTML = "西";
-			document.getElementById("p4wind").innerHTML = "北";
-			break;
-		case 2:
-			document.getElementById("p2wind").innerHTML = "東";
-			document.getElementById("p3wind").innerHTML = "南";
-			document.getElementById("p4wind").innerHTML = "西";
-			document.getElementById("p1wind").innerHTML = "北";
-			break;
-		case 3:
-			document.getElementById("p3wind").innerHTML = "東";
-			document.getElementById("p4wind").innerHTML = "南";
-			document.getElementById("p1wind").innerHTML = "西";
-			document.getElementById("p2wind").innerHTML = "北";
-			break;
-		case 4:
-			document.getElementById("p4wind").innerHTML = "東";
-			document.getElementById("p1wind").innerHTML = "南";
-			document.getElementById("p2wind").innerHTML = "西";
-			document.getElementById("p3wind").innerHTML = "北";
-			break;
-		default: //horrible failure
-			break;
+		switch(kyoku)
+		{
+			case 1:
+				document.getElementById("p1wind").innerHTML = "東";
+				document.getElementById("p2wind").innerHTML = "南";
+				document.getElementById("p3wind").innerHTML = "西";
+				break;
+			case 2:
+				document.getElementById("p2wind").innerHTML = "東";
+				document.getElementById("p3wind").innerHTML = "南";
+				document.getElementById("p1wind").innerHTML = "西";
+				break;
+			case 3:
+				document.getElementById("p3wind").innerHTML = "東";
+				document.getElementById("p1wind").innerHTML = "南";
+				document.getElementById("p2wind").innerHTML = "西";
+				break;
+			default: //horrible failure
+				break;
+		}
+	}
+	else
+	{
+		switch(kyoku)
+		{
+			case 1:
+				document.getElementById("p1wind").innerHTML = "東";
+				document.getElementById("p2wind").innerHTML = "南";
+				document.getElementById("p3wind").innerHTML = "西";
+				document.getElementById("p4wind").innerHTML = "北";
+				break;
+			case 2:
+				document.getElementById("p2wind").innerHTML = "東";
+				document.getElementById("p3wind").innerHTML = "南";
+				document.getElementById("p4wind").innerHTML = "西";
+				document.getElementById("p1wind").innerHTML = "北";
+				break;
+			case 3:
+				document.getElementById("p3wind").innerHTML = "東";
+				document.getElementById("p4wind").innerHTML = "南";
+				document.getElementById("p1wind").innerHTML = "西";
+				document.getElementById("p2wind").innerHTML = "北";
+				break;
+			case 4:
+				document.getElementById("p4wind").innerHTML = "東";
+				document.getElementById("p1wind").innerHTML = "南";
+				document.getElementById("p2wind").innerHTML = "西";
+				document.getElementById("p3wind").innerHTML = "北";
+				break;
+			default: //horrible failure
+				break;
+		}
 	}
 
 	//player points
@@ -73,9 +123,35 @@ function update_display()
 	document.getElementById("p3points").innerHTML = points["West"];
 	document.getElementById("p4points").innerHTML = points["North"];
 	document.getElementById("riichipoints").innerHTML = Number(riichis)*1000;
+
+	if(sanma)
+		document.getElementById("northdisplay").classList.add("hidediv");
+	else
+		document.getElementById("northdisplay").classList.remove("hidediv");
 }
 
-function check_over()
+function kyoku_increase()
+{
+	kyoku++;
+	if(sanma)
+	{
+		if(kyoku > 3)
+		{
+			round++;
+			kyoku=1;
+		}
+	}
+	else
+	{
+		if(kyoku > 4)
+		{
+			round++;
+			kyoku=1;
+		}
+	}
+}
+
+function check_over() //asked just AFTER a win/exhaust (eastwon should still show correct state)
 {
 	//points < 0 OR
 	//last dealer lost or not tenpai && anyone > 30k OR
@@ -85,7 +161,7 @@ function check_over()
 
 function startup()
 {
-	stage = 1;
+	stage = 0;
 	display_stage();
 }
 
@@ -109,11 +185,33 @@ function display_stage()
 	var sels = document.getElementsByTagName("SELECT");
 	for(var i=0;i<sels.length;++i)
 		sels.selectedIndex = "0";
+
+	var togs = document.getElementsByClassName("tenpaitoggle");
+	for(var i=0;i<togs.length;++i)
+		togs[i].classList.remove("selected");
 }
 
 function change_stage(s)
 {
 	stage = s;
+	if(s == 0)
+	{
+		players={"East":"None","South":"None","West":"None","North":"None"};
+		points={"East":25000,"South":25000,"West":25000,"North":25000};
+
+		sanma=false;
+
+		tonpuu=false;
+		eastwon=false;
+		tempriichis=0;
+		atamahane=0;
+
+		riichis=0;
+		honba=0;
+		round=0; 
+		kyoku=1;
+
+	}
 	display_stage();
 }
 
@@ -142,10 +240,10 @@ function commonstart()
 	if(north.selectedIndex == "0")
 		sanma=true;
 
-	players["east"] = east.options[east.selectedIndex].text;
-	players["south"] = south.options[south.selectedIndex].text;
-	players["west"] = west.options[west.selectedIndex].text;
-	players["north"] = north.options[north.selectedIndex].text;
+	players["East"] = east.options[east.selectedIndex].text;
+	players["South"] = south.options[south.selectedIndex].text;
+	players["West"] = west.options[west.selectedIndex].text;
+	players["North"] = north.options[north.selectedIndex].text;
 
 	var sels = [document.getElementById("winner"),document.getElementById("loser"),document.getElementById("aborter"),document.getElementById("chomboer")];
 	
@@ -162,7 +260,47 @@ function commonstart()
 	document.getElementById("p3name").innerHTML = players["West"];
 	document.getElementById("p4name").innerHTML = players["North"];
 
+	document.getElementById("winrieast").innerHTML = players["East"];
+	document.getElementById("winrisouth").innerHTML = players["South"];
+	document.getElementById("winriwest").innerHTML = players["West"];
+	document.getElementById("winrinorth").innerHTML = players["North"];
+	document.getElementById("abrieast").innerHTML = players["East"];
+	document.getElementById("abrisouth").innerHTML = players["South"];
+	document.getElementById("abriwest").innerHTML = players["West"];
+	document.getElementById("abrinorth").innerHTML = players["North"];
+	document.getElementById("exrieast").innerHTML = players["East"];
+	document.getElementById("exrisouth").innerHTML = players["South"];
+	document.getElementById("exriwest").innerHTML = players["West"];
+	document.getElementById("exrinorth").innerHTML = players["North"];
+
+	if(sanma)
+	{
+		points["East"]=30000;
+		points["South"]=30000;
+		points["West"]=30000;
+		points["North"]=0;
+	}
+
+	time=0;
+	interval = window.setInterval(update_time,1000);
+
+	update_display();
 	change_stage(1);
+}
+
+function pausebutton()
+{
+	var btn = document.getElementById("pausebutton");
+	if(btn.innerHTML == "pause_circle_outline")
+	{
+		btn.innerHTML = "play_circle_outline";
+		window.clearInterval(interval);
+	}
+	else
+	{
+		btn.innerHTML = "pause_circle_outline";
+		interval = window.setInterval(update_time,1000);
+	}
 }
 
 function winbutton()
@@ -288,6 +426,8 @@ function commonwin()
 		{
 			if(i+1 == winner)
 				continue;
+			if(i == 3 && sanma)
+				continue;
 
 			var eastloss = (document.getElementById("p"+Number(i+1)+"wind").innerHTML == "東");
 			var tpay = roundpay(payment);
@@ -309,13 +449,35 @@ function commonwin()
 		payment = roundpay(payment);
 		payment += 300*honba;
 		points[playwinds[winner-1]] += payment;
-		points[playwinds[loser-1]] += payment;
+		points[playwinds[loser-1]] -= payment;
+	}
+
+	//riichis this round
+	if(document.getElementById("winrieast").classList.contains("selected") && winwind != "東")
+	{
+		++riichis;
+		points["East"] -= 1000;
+	}
+	if(document.getElementById("winrisouth").classList.contains("selected") && winwind != "南")
+	{
+		++riichis;
+		points["South"] -= 1000;
+	}
+	if(document.getElementById("winriwest").classList.contains("selected") && winwind != "西")
+	{
+		++riichis;
+		points["West"] -= 1000;
+	}
+	if(document.getElementById("winrinorth").classList.contains("selected") && winwind != "北" && !sanma)
+	{
+		++riichis;
+		points["North"] -= 1000;
 	}
 
 	//handle riichi sticks
 	if(riichis > 0)
 	{
-		tempriichi = riichis;
+		tempriichis = riichis;
 		riichis = 0;
 	}
 	if(tsumo)
@@ -339,18 +501,11 @@ function windonebutton()
 
 	//handle riichi atamahanes
 	var playwinds = ["East","South","West","North"];
-	if(tempriichi > 0)
+	if(tempriichis > 0)
 	{
-		points[playwinds[atamahane-1]] += tempriichi*1000;
+		points[playwinds[atamahane-1]] += tempriichis*1000;
 	}
 
-	else
-	{
-		eastwon = false;
-		atamahane = 0;
-		update_display();
-		change_stage(1);
-	}
 
 	if(check_over())
 	{
@@ -362,13 +517,13 @@ function windonebutton()
 		honba++;
 	else
 	{
-		++kyoku;
-		if(kyoku > 4)
-		{
-			kyoku = 1;
-			++round;
-		}
+		kyoku_increase();
 	}
+
+	eastwon = false;
+	atamahane = 0;
+	update_display();
+	change_stage(1);
 }
 
 function winanotherbutton()
@@ -386,16 +541,127 @@ function winanotherbutton()
 
 function exhaustdonebutton()
 {
+	//tenpais
+	var tenpaicount = 0;
+	var tpeast = document.getElementById("tpeast").classList.contains("selected");
+	var tpsouth = document.getElementById("tpsouth").classList.contains("selected");
+	var tpwest = document.getElementById("tpwest").classList.contains("selected");
+	var tpnorth = document.getElementById("tpnorth").classList.contains("selected");
+
+	if((tpeast && document.getElementById("p1wind").innerHTML == "東"))
+		eastwon = true;
+	if((tpsouth && document.getElementById("p1wind").innerHTML == "東"))
+		eastwon = true;
+	if((tpwest && document.getElementById("p1wind").innerHTML == "東"))
+		eastwon = true;
+	if((tpnorth && document.getElementById("p1wind").innerHTML == "東"))
+		eastwon = true;
+
+	if(sanma)
+	{
+		tenpaicount = (tpeast?1:0)+(tpsouth?1:0)+(tpwest?1:0);
+		var pay = 0;
+		if(tenpaicount == 1)
+			pay = 1000;
+		else if(tenpaicount == 2)
+			pay = 2000;
+
+		var get = (pay*(3-tenpaicount))/(tenpaicount);
+		if(tpeast) { points["East"] += get; } else { points["East"] -= pay; }
+		if(tpsouth) { points["South"] += get; } else { points["South"] -= pay; }
+		if(tpwest) { points["West"] += get; } else { points["West"] -= pay; }
+
+	}
+	else
+	{
+		tenpaicount = (tpeast?1:0)+(tpsouth?1:0)+(tpwest?1:0)+(tpnorth?1:0);
+		var pay = 0;
+		if(tenpaicount == 1)
+			pay = 1000;
+		else if(tenpaicount == 2)
+			pay = 1500;
+		else if(tenpaicount == 3)
+			pay = 3000;
+
+		var get = (pay*(4-tenpaicount))/(tenpaicount);
+		if(tpeast) { points["East"] += get; } else { points["East"] -= pay; }
+		if(tpsouth) { points["South"] += get; } else { points["South"] -= pay; }
+		if(tpwest) { points["West"] += get; } else { points["West"] -= pay; }
+		if(tpnorth) { points["North"] += get; } else { points["North"] -= pay; }
+	}
+
+	//riichis this round
+	if(document.getElementById("exrieast").classList.contains("selected"))
+	{
+		++riichis;
+		points["East"] -= 1000;
+	}
+	if(document.getElementById("exrisouth").classList.contains("selected"))
+	{
+		++riichis;
+		points["East"] -= 1000;
+	}
+	if(document.getElementById("exriwest").classList.contains("selected"))
+	{
+		++riichis;
+		points["East"] -= 1000;
+	}
+	if(document.getElementById("exrinorth").classList.contains("selected") && !sanma)
+	{
+		++riichis;
+		points["East"] -= 1000;
+	}
+
+	if(check_over())
+	{
+		change_stage(6);
+		return;
+	}
+
+
+	++honba;
+	if(eastwon)
+		eastwon = false;
+	else
+	{
+		kyoku_increase();
+	}
+	update_display();
 	change_stage(1);
 }
 
 function abortdonebutton()
 {
+	++honba;
+
+	//riichis this round
+	if(document.getElementById("abrieast").classList.contains("selected"))
+	{
+		++riichis;
+		points["East"] -= 1000;
+	}
+	if(document.getElementById("abrisouth").classList.contains("selected"))
+	{
+		++riichis;
+		points["East"] -= 1000;
+	}
+	if(document.getElementById("abriwest").classList.contains("selected"))
+	{
+		++riichis;
+		points["East"] -= 1000;
+	}
+	if(document.getElementById("abrinorth").classList.contains("selected"))
+	{
+		++riichis;
+		points["East"] -= 1000;
+	}
+	update_display();
 	change_stage(1);
 }
 
 function chombodonebutton()
 {
+	chombos[Number(document.getElementById("chomboer").selectedIndex)-1]++;
 	change_stage(1);
 }
 
